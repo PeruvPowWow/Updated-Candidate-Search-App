5e622bb925ce5e65233e50cd5e354621957fcfe7
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-11 13:20:50 -0600

    added to app file

--- Develop /src/App.tsx
+++ Develop /src/App.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CandidateSearch from './pages/CandidateSearch';
+import SavedCandidates from './pages/SavedCandidates';
 import Nav from './components/Nav';
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
       <main>
-        <Outlet />
+        <Routes>
+          <Route path="/" element={<CandidateSearch />} />
+          <Route path="/saved-candidates" element={<SavedCandidates />} />
+        </Routes> 
       </main>
-    </>
+    </Router>
   );
 }
 
