6e5a2a2d6efd9cf632d4a2760a7153161019345c
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-11 11:19:30 -0600

    added the interface code to the candidate interface file

--- Develop /src/interfaces/Candidate.interface.tsx
+++ Develop /src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string;        // The GitHub username of the candidate
+    avatar_url: string;   // The URL of the candidate's avatar image
+    name: string;         // The full name of the candidate
+    location?: string;    // The location of the candidate (optional field)
+    email?: string;       // The email address of the candidate (optional field)
+    html_url: string;     // The URL to the candidate's GitHub profile
+    company?: string;     // The company the candidate works for (optional field)
+  }
